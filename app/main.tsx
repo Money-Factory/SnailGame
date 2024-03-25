273976c117827638ca4a6270ab25530ec979db54
Derek Boyer <20boyerd@gmail.com>
2024-03-24 23:10:04 -0700

    added safeareaview to main

--- app/main.tsx
+++ app/main.tsx
@@ -1,27 +1,43 @@
 import React from "react";
-import { StyleSheet, View, ImageBackground } from "react-native";
+import { StyleSheet, View, ImageBackground, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import background from "../assets/images/road.png";
 
 const Main: React.FC = () => {
   return (
-    <View style={styles.container}>
+    <View style={styles.root}>
       <ImageBackground
         source={background}
-        resizeMode="cover"
+        resizeMode="stretch"
         style={styles.background}
-      />
+      >
+        <SafeAreaView style={styles.root}>
+          <View style={styles.test2}>
+            <Text>filler</Text>
+          </View>
+        </SafeAreaView>
+      </ImageBackground>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
   },
   background: {
     flex: 1,
     justifyContent: "center",
+  },
+  test: {
+    flex: 1,
+    backgroundColor: "red",
+  },
+  test2: {
+    flex: 1,
+    borderColor: "yellow",
+    borderWidth: 1,
   },
 });
 
