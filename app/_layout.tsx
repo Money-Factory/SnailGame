b433e730e8a33b1cfd101de54e313aa0b2c6b3b9
Derek Boyer <20boyerd@gmail.com>
2024-03-24 17:52:35 -0700

    added main page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -18,7 +18,7 @@ export {
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "home",
+  initialRouteName: "main",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
---
+++ app/main.tsx
@@ -0,0 +1,26 @@
+import { Link } from "expo-router";
+import React from "react";
+import { StyleSheet, View, Text, Pressable } from "react-native";
+
+const Main: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to snail game</Text>
+      <Link href="/tutorial" asChild>
+        <Pressable>
+          <Text>Click for tutorial started!!!!!!!!!</Text>
+        </Pressable>
+      </Link>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default Main;
